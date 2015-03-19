ac9e69f9bb5c270305b671f36ef9c4a6fa8a8054
fitorec <chanerec@gmail.com>
2015-03-19 13:13:28 -0600

    Sendding email with ssl

--- src/main/java/com/mundosica/core/Utils/Gmail.java
+++ src/main/java/com/mundosica/core/Utils/Gmail.java
@@ -1,8 +1,10 @@
 package com.mundosica.core.Utils;
 
 import java.util.Properties;
+
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
@@ -57,51 +59,59 @@ public class Gmail {
 			}
 		}
 	}
- public static Boolean send(String... namesAndValues) {
-	 Gmail.config(namesAndValues);
-	 try {
-		return Gmail.send();
-	} catch (MessagingException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public static Boolean send(String... namesAndValues) {
+		Gmail.config(namesAndValues);
+		try {
+			return Gmail.send();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
 	}
-	 return false;
- }
- 	public static Boolean send() throws AddressException, MessagingException {
- 	Boolean success = false;
-	mailServerProperties = System.getProperties();
-	mailServerProperties.put("mail.smtp.port", "587");
-	mailServerProperties.put("mail.smtp.auth", "true");
-	mailServerProperties.put("mail.smtp.starttls.enable", "true");
-	getMailSession = Session.getDefaultInstance(mailServerProperties, null);
-	generateMailMessage = new MimeMessage(getMailSession);
-	if (Gmail.to != null) {
-		generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(Gmail.to));
-	}
-	if (Gmail.cc != null) {
-		generateMailMessage.addRecipient(Message.RecipientType.CC, new InternetAddress(Gmail.cc));
-	}
-	if (Gmail.bcc != null) {
-		generateMailMessage.addRecipient(Message.RecipientType.BCC, new InternetAddress(Gmail.bcc));
-	}
-	if (Gmail.subject == null) {
-		Gmail.subject = "subject gmail object";
-	}
-	generateMailMessage.setSubject(Gmail.subject);
-	if (Gmail.body == null) {
-		Gmail.body = "<h1>body gmail object</h1><p>it's a simple test</p>";
-	}
-	generateMailMessage.setContent(Gmail.body, "text/html");
-	//Step3
-	Transport transport = getMailSession.getTransport("smtp");
-	// Enter your correct gmail UserID and Password (XXXApp Shah@gmail.com)
-	if (Gmail.login == null || Gmail.password == null) {
+	public static Boolean send() throws AddressException, MessagingException {
+		Boolean success = false;
+		// step 1 set connection properties
+		mailServerProperties = System.getProperties();
+		mailServerProperties.put("mail.smtp.host", "smtp.gmail.com");
+		mailServerProperties.put("mail.smtp.socketFactory.port", "465");
+		mailServerProperties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		mailServerProperties.put("mail.smtp.auth", "true");
+		mailServerProperties.put("mail.smtp.port", "465");
+		// step 2 Authentication
+		if (Gmail.login == null || Gmail.password == null) {
+				return success;
+		}
+		Session getMailSession = Session.getDefaultInstance(mailServerProperties,new javax.mail.Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+			return new PasswordAuthentication(Gmail.login, Gmail.password);
+			}
+		});
+
+		// step 3 sending Email
+		generateMailMessage = new MimeMessage(getMailSession);
+		if (Gmail.to != null) {
+			generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(Gmail.to));
+		}
+		if (Gmail.cc != null) {
+			generateMailMessage.addRecipient(Message.RecipientType.CC, new InternetAddress(Gmail.cc));
+		}
+		if (Gmail.bcc != null) {
+			generateMailMessage.addRecipient(Message.RecipientType.BCC, new InternetAddress(Gmail.bcc));
+		}
+		if (Gmail.subject == null) {
+			Gmail.subject = "subject gmail object";
+		}
+		generateMailMessage.setSubject(Gmail.subject);
+		if (Gmail.body == null) {
+			Gmail.body = "<h1>body gmail object</h1><p>it's a simple test</p>";
+		}
+		generateMailMessage.setContent(Gmail.body, "text/html");
+		Transport transport = getMailSession.getTransport("smtps");
+		Transport.send(generateMailMessage);
+		transport.close();
 		return success;
 	}
-	transport.connect("smtp.gmail.com", Gmail.login, Gmail.password);
-	transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
-	transport.close();
-	return success;
-}
 
 }
